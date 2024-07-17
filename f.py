229bee643d1b4c769734f8423d3440950f500415
David Tauraso <david318@me.com>
2024-07-16 23:50:22 -0700

    Updated code

--- f.py
+++ f.py
@@ -3,6 +3,9 @@ class Point:
     def __init__(self, line_id, point_id):
         self.line_id = line_id
         self.point_id = point_id
+        self.next = None
+        self.prev = None
+        self.parent = None
 
 
 def x():
@@ -21,8 +24,11 @@ def x():
             if current_item == item:
                 streak_count += 1
             else:
-                if streak_count > 0:
-                    new_sequence.append(streak_count)
+                if streak_count == 0:
+                    streak_count = 1
+                    current_item = item
+                    continue
+                new_sequence.append(streak_count)
                 streak_count = 1
                 current_item = item
         if streak_count > 0:
@@ -33,4 +39,35 @@ def x():
         new_sequence = []
         print(sequence)
 
-x()
+def x2():
+
+    sequence = [1, 1, 1]
+
+
+    lines = {}
+    
+    tracker = {}
+    streak_count = 0
+    current_item = 0
+    new_sequence = []
+    print(sequence)
+
+    for _, item in enumerate(sequence):
+        if current_item == item:
+            streak_count += 1
+        if item not in lines:
+            point = Point(line_id=item, point_id=0)
+            lines[item] = [point]
+            tracker[item] = point
+        else:
+            if tracker[item].next == None:
+                tracker[item].next = Point(line_id=item, point_id=len(lines[item]))
+                tracker[item].next.prev = lines[item][-1]
+                lines[item][-1].next = tracker[item].next
+                lines[item].append(tracker[item].next)
+            tracker[item] = tracker[item].next
+
+    # if streak_count > 0:
+    # print(sequence)
+
+x2()
