3b499d39b7aee8b24e74d494f1de178f218240f8
David Tauraso <david318@me.com>
2024-07-19 01:18:17 -0700

    Updated code

--- f.py
+++ f.py
@@ -1,7 +1,8 @@
 
 class Point:
-    def __init__(self, line_id, point_id):
+    def __init__(self, line_id, section_id, point_id):
         self.line_id = line_id
+        self.section_id = section_id
         self.point_id = point_id
         self.is_active = False
         self.sequence_id = 0
@@ -15,9 +16,9 @@ class Point:
         child_coordinates = self.child.returnCoordinates() if self.child != None else None
         next_coordinates = self.next.returnCoordinates() if self.next != None else None
         prev_coordinates = self.prev.returnCoordinates() if self.prev != None else None
-        return f"(line id: {self.line_id}, point id: {self.point_id}, parent: {parent_coordinates}, next: {next_coordinates}, prev: {prev_coordinates}, child: {child_coordinates})"
+        return f"(line id: {self.line_id}, section id: {self.section_id}, point id: {self.point_id}, parent: {parent_coordinates}, next: {next_coordinates}, prev: {prev_coordinates}, child: {child_coordinates})"
     def returnCoordinates(self):
-        return (self.line_id, self.point_id)
+        return (self.line_id, self.section_id, self.point_id)
 
 def x():
 
@@ -102,6 +103,64 @@ def x2():
         print(item)
         [print(str(point)) for point in lines[item]]
         print()
-    # print(sequence)
 
-x2()
+def x3():
+
+    sequence = [1, 1, 1]
+
+
+    lines = {}
+    
+    tracker = None
+    streak_count = 0
+    current_item = 0
+    new_sequence = []
+    print(sequence)
+
+    for i, item in enumerate(sequence):
+        if item not in lines:
+            point = Point(line_id=item, section_id=0, point_id=0)
+            lines[item] = [[point]]
+            tracker = point
+            streak_count = 1
+        else:
+            streak_count += 1
+            if tracker.next == None:
+                section_id = len(lines[item])-1
+                tracker.next = Point(line_id=item, section_id=section_id, point_id=len(lines[item][section_id]))
+                lines[item][section_id][-1].next = Point(line_id=tracker.next.line_id, section_id=section_id, point_id=tracker.next.point_id)
+                lines[item][section_id][-1].next.prev = Point(line_id=lines[item][section_id][-1].line_id, section_id=section_id, point_id=lines[item][section_id][-1].point_id)
+                lines[item][section_id].append(tracker.next)
+            if i >= len(sequence)-1:
+                continue
+            if tracker.next.line_id == sequence[i+1]:
+                tracker = tracker.next
+    if streak_count > 0:
+        tracker = tracker.next            
+        tracker.parent = Point(line_id=streak_count, section_id=0, point_id=0)
+        while tracker.parent.line_id > 0:
+            if streak_count not in lines:
+                lines[streak_count] = [[tracker.parent]]
+            elif streak_count in lines:
+                tracker.parent.point_id = 0
+                tracker.parent.section_id = len(lines[streak_count])
+                lines[streak_count].append([tracker.parent])
+            parent_line = tracker.parent
+            parent_line.child = Point(line_id=tracker.line_id, section_id=tracker.section_id, point_id=tracker.point_id)
+            parent_line.child.parent = Point(line_id=parent_line.line_id, section_id=section_id, point_id=parent_line.point_id)
+            if streak_count == 1:
+                break
+            tracker = tracker.parent
+            streak_count = 1
+            section_id = len(lines[streak_count])
+            tracker.parent = Point(line_id=1, section_id=section_id, point_id=parent_line.point_id)
+
+    for line in lines:
+        print(f"line {line}")
+        for i, section in enumerate(lines[line]):
+            print(f"{i}")
+            for j, point in enumerate(section):
+                print(f"    {j} {str(point)}")
+        print()
+
+x3()
