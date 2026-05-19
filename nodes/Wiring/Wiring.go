c6f7d239fb1a8918769f4cd06a1205ee7d81b617
David Tauraso <david318@me.com>
2026-05-19 01:07:23 -0700

    rename(wiring): i0Test → i0 (drop fixture-leaked suffix)

--- nodes/Wiring/Wiring.go
+++ nodes/Wiring/Wiring.go
@@ -5,16 +5,16 @@
 // Nodes:
 //   in08           Input
 //   readGate1      ReadGate
-//   i0Test         ChainInhibitor
+//   i0             ChainInhibitor
 //   i1             ChainInhibitor
 //   inhibitRight0  InhibitRightGate
 //
 // Edges:
-//   readGateToI0      readGate1.out -> i0Test.in
-//   i0ToI1            i0Test.out -> i1.in
+//   readGateToI0      readGate1.out -> i0.in
+//   i0ToI1            i0.out -> i1.in
 //   i1AckToReadGate   i1.out -> readGate1.ack
 //   inputToReadGate   in08.out -> readGate1.chainIn
-//   i0ToInhibitRight  i0Test.inhibitOut -> inhibitRight0.left
+//   i0ToInhibitRight  i0.inhibitOut -> inhibitRight0.left
 //   i1ToInhibitRight  i1.inhibitOut -> inhibitRight0.right
 
 package Wiring
@@ -46,16 +46,16 @@ func Wire() []S.Node {
 	in08Input <- 0
 
 	// Dead-end outputs
-	i0TestAck := make(chan int, 1)
+	i0Ack := make(chan int, 1)
 	i1Ack := make(chan int, 1)
 	inhibitRight0Out := make(chan int, 1)
 
 	// Nodes
 	in08 := INN.InputNode{Id: 0, Name: "in08", Input: in08Input, ToNext: inputToReadGate}
 	readGate1 := RGN.ReadGateNode{Id: 0, Name: "readGate1", FromAck: i1AckToReadGate, FromValue: inputToReadGate, ToLatch: readGateToI0}
-	i0Test := CI.ChainInhibitorNode{Id: 0, Name: "i0Test", FromPrev: readGateToI0, ToAck: i0TestAck, ToEdge: []chan<- int{i0ToInhibitRight}, ToNext: i0ToI1}
+	i0 := CI.ChainInhibitorNode{Id: 0, Name: "i0", FromPrev: readGateToI0, ToAck: i0Ack, ToEdge: []chan<- int{i0ToInhibitRight}, ToNext: i0ToI1}
 	i1 := CI.ChainInhibitorNode{Id: 1, Name: "i1", FromPrev: i0ToI1, ToAck: i1Ack, ToEdge: []chan<- int{i1ToInhibitRight}, ToNext: i1AckToReadGate}
 	inhibitRight0 := IRG.InhibitRightGateNode{Id: 0, Name: "inhibitRight0", FromLeft: i0ToInhibitRight, FromRight: i1ToInhibitRight, ToOut: inhibitRight0Out}
 
-	return []S.Node{&in08, &readGate1, &i0Test, &i1, &inhibitRight0}
+	return []S.Node{&in08, &readGate1, &i0, &i1, &inhibitRight0}
 }
